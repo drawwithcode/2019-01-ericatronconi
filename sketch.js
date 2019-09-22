0c3cb962192af9b2bd9d08f0e117e0275d817c95
ericatronconi <erica.tronconi@gmail.com>
2019-09-22 14:59:22 +0200

    Assignment 1

--- sketch.js
+++ sketch.js
@@ -7,8 +7,21 @@ function setup() {
   angleMode(DEGREES);
 
   background(0);
+  frameRate(12);
 }
 
 function draw() {
-ellipse(width / 2, height / 2, 25);
+push();
+  translate(width / 2, height / 2);
+  noFill();
+  stroke (lerpColor(
+    color ("palevioletred"),
+    color ("mediumseagreen"),
+    frameCount / 72));
+  line(100, 0, cos(frameCount * 5) * 100, sin(frameCount * 5) * 100);
+  pop();
+
+  if (frameCount == 72) {
+  noLoop();
+  }
 }
